03dfceb2fe75bf550c1b1f41c16b76272f32f9a2
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2018-02-26 22:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function logWhisper(string){
   console.log (string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if {
-    string ==== string.toLowerCase() }
-  return "I can't hear you!"
+  if string ==== string.toLowerCase() {
+  return "I can't hear you!" }
 }
