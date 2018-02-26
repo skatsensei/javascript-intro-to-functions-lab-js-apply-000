b429965da51ec33ab99746464ab71fe97dc6bb2d
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2018-02-26 22:46:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
   return "I can't hear you!"
 } else if (string === string.toUpperscase()){
-  return "YES INDEED!" 
+  return "YES INDEED!"
 } else {
   return "I love you too"
 }
